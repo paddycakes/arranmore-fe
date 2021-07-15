2ee2717ff12521de88e1da59412fbc6d21feca9a
Paddy Gallagher <paddy.gallagher@gmail.com>
2021-07-15 22:00:15 +0100

    Adding Google AppEngine configuration

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,18 +2,13 @@ const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/app.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build', 'js'),
     publicPath: 'build/js',
   },
-  // For sourcemaps
-  devtool: 'cheap-module-eval-source-map',
-  // The below is default so not strictly necessary
-  devServer: {
-    contentBase: './',
-  },
+  devtool: 'cheap-source-map',
   plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
