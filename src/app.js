cefbfb15342f0e189ca11583521ecde572138911
smbonner <seamus@iimro.org>
2021-08-08 11:27:09 +0100

    Refactor to use real device ID

--- src/app.js
+++ src/app.js
@@ -1,18 +1,48 @@
 import { fetchSensorMetrics } from './services/api';
 
 const btnLogin = document.querySelector('.login__btn');
-const inputLoginClientID = document.querySelector('.login__input--deviceid');
+const deviceIdInput = document.querySelector('.login__input--deviceid');
+
+const account1 = {
+  owner: 'SeamusBonner',
+};
+
+// const user = 'Seamus Bonner'; //sb
+// const username = account1.owner
+//   .toLowerCase()
+//   .split(' ')
+//   .map((name) => name[0])
+//   .join('');
+
+console.log(account1.owner);
+const password = {
+  id: 'arranmoreIoT',
+};
+
+const deviceId = deviceIdInput.value;
+
+//Base64 Handler
+const userPass = `${account1.owner}:${password.id}`;
+const apiKey = btoa(userPass);
 
 btnLogin.addEventListener('click', async function (e) {
   //prevent form submitting
   e.preventDefault();
 
-  const loginClientId = inputLoginClientID.value;
+  console.log(deviceId);
 
-  if (loginClientId !== '') {
-    const metrics = await fetchSensorMetrics(loginClientId);
+  if (deviceId !== '') {
+    console.log(apiKey);
+    console.log(userPass);
+
+    const metrics = await fetchSensorMetrics(deviceId, apiKey);
+
     console.log(`Sensor metrics: ${JSON.stringify(metrics)}`);
   } else {
-    alert('Please enter a value in the Device ID field ');
+    document.querySelector('.login__input--deviceid').value = 'Incorrect ID';
+    // alert('Incorrect Password!');
   }
 });
+console.log(apiKey);
+console.log(userPass);
+console.log(deviceId);
