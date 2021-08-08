f92a9f648ff90242a9b5bed378395fff2f1518e7
Paddy Gallagher <paddy.gallagher@gmail.com>
2021-08-08 14:33:08 +0100

    Dynamic generation of api key

--- src/app.js
+++ src/app.js
@@ -3,36 +3,21 @@ import { fetchSensorMetrics } from './services/api';
 const btnLogin = document.querySelector('.login__btn');
 const deviceIdInput = document.querySelector('.login__input--deviceid');
 
-const account1 = {
+const account = {
   owner: 'SeamusBonner',
+  pw: 'arranmoreIoT',
 };
-
-// const user = 'Seamus Bonner'; //sb
-// const username = account1.owner
-//   .toLowerCase()
-//   .split(' ')
-//   .map((name) => name[0])
-//   .join('');
-
-console.log(account1.owner);
-const password = {
-  id: 'arranmoreIoT',
-};
-
-//Base64 Handler
-const userPass = `${account1.owner}:${password.id}`;
-const apiKey = btoa(userPass);
 
 btnLogin.addEventListener('click', async function (e) {
   //prevent form submitting
   e.preventDefault();
 
   const deviceId = deviceIdInput.value;
-  console.log(deviceId);
+  const apiKey = getApiKey();
 
   if (deviceId !== '') {
+    console.log(deviceId);
     console.log(apiKey);
-    console.log(userPass);
 
     const metrics = await fetchSensorMetrics(deviceId, apiKey);
     console.log(`Sensor metrics: ${JSON.stringify(metrics)}`);
@@ -40,6 +25,8 @@ btnLogin.addEventListener('click', async function (e) {
     document.querySelector('.login__input--deviceid').value = 'Incorrect ID';
   }
 });
-console.log(apiKey);
-console.log(userPass);
-console.log(deviceId);
+
+const getApiKey = () => {
+  const userCreds = `${account.owner}:${account.pw}`;
+  return btoa(userCreds);
+};
--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,19 @@
 import environment from '../environment';
 const baseUrl = environment.baseUrl;
 
+// TODO: Handle apiKey more robustly
 export const fetchSensorMetrics = async (deviceId, apiKey) => {
   const headers = {
     'Content-Type': 'application/json',
   };
 
   try {
-    const response = await fetch(`${baseUrl}/api/sensor/${deviceId}/metrics`, {
-      headers,
-    });
+    const response = await fetch(
+      `${baseUrl}/api/${apiKey}/sensor/${deviceId}/metrics`,
+      {
+        headers,
+      }
+    );
     const metrics = await response.json();
     return metrics;
   } catch (e) {
